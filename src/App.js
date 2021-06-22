ddb11ddfd9fe5c80751e4d59ea7db74367b16704
Josh Rehanek <joshrehanek@gmail.com>
2021-06-21 21:55:21 -0700

    sets up App.js w/ Router, Switch, Nav, & Home Comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import Nav from "./components/Navbar/Nav"
+import Home from "./components/pages/Home"
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
