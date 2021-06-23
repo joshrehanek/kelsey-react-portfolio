dbd99695bd9ce8d5521bc50d030719d8756224ba
Josh Rehanek <joshrehanek@gmail.com>
2021-06-22 23:43:07 -0700

    adds vars to set states

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,3 +1,11 @@
 import React, { useState } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import './Nav.css';
+
+export default function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    
+}
