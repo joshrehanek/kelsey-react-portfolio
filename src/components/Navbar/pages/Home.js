2424278f3d08068288cc887c01bb39f267f00b53
Josh Rehanek <joshrehanek@gmail.com>
2021-06-28 22:34:00 -0700

    adds Home.js to pages

---
+++ src/components/Navbar/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "../../styles.css";
+// import Cards from '../Cards/Cards';
+// import Hero from "../Hero/Hero";
+// import Footer from "../Footer/Footer"
+// import Bio from "../Bio/Bio"
+
+export default function Home () {
+    return (
+        <div>
+            stuff and things
+            {/* <Hero />
+            <Bio />
+            <Cards />
+            <Footer /> */}
+        </div>
+    )
+}
+
