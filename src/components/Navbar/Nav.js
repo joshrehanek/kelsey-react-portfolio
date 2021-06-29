692ac6a9f1a0eac693f549d556eaa32adb169941
Josh Rehanek <joshrehanek@gmail.com>
2021-06-28 22:18:38 -0700

    adds onClick to handleClick event in NavBar

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -7,15 +7,22 @@ export default function Navbar() {
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    
+
     return (
         <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                    KCS
-                </Link>
-            </div>
-        </nav>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        KCS
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            
+                    </ul>
+                </div>
+            </nav>
+        </>
     )
 }
