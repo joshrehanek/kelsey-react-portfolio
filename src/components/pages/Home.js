8af122c3553c35132f89d1f0b2761055a4120bf2
Josh Rehanek <joshrehanek@gmail.com>
2021-06-30 00:09:08 -0700

    adds contact section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "../../index.css";
 import Hero from "../Hero/Hero";
-// import Bio from "../Bio/Bio"
+import Bio from "../Bio/Bio";
+import Contact from "../Contact/Contact";
+
 
 export default function Home () {
     return (
         <div>
             <Hero />
-            {/* <Bio /> */}
-     
+            <Bio />
+            <Contact />
         </div>
     )
 }
