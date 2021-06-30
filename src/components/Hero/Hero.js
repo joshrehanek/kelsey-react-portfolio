d088a7283ab01e21034a38b1dafbc1c9e4532614
Josh Rehanek <joshrehanek@gmail.com>
2021-06-29 21:57:44 -0700

    adds initial hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,3 +1,33 @@
 import React from "react";
-import "../../styles.css"
+import "../../index.css"
 import "./Hero.css"
+
+export default function Hero() {
+    return (
+        <div className='hero'>
+            <div className='hero-container'>
+                <section class="banner" id="home">
+                    <div class="text-box">
+                        <h2>Kelsey Camden Smith</h2>
+                        <h3>Behavioral Health Therapist</h3>
+                        <br>
+                        </br>
+                        < img 
+                            src={}
+                        {/* <a
+                            class="link"
+                            href={}
+                            download
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <img
+                                src="https://img.icons8.com/nolan/64/open-resume.png"
+                                alt="download resume" />
+                        </a> */}
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+}
