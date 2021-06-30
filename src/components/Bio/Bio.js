47e52bca83dca9a52f806c1e23b617d5f51d1a23
Josh Rehanek <joshrehanek@gmail.com>
2021-06-30 00:09:59 -0700

    adds initial bio section

---
+++ src/components/Bio/Bio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Bio.css';
+
+export default function Bio() {
+    return (
+        <div id="bio" className="bio-container">
+                <div class="heading">
+                    <h2>Bio</h2>
+                </div>
+                <div>
+                    <h5 class="bio-content">Nam quis leo quam. Proin eget tristique purus, eu cursus eros. Etiam ullamcorper eleifend velit id sollicitudin. Curabitur pellentesque neque ligula, et tristique libero fringilla id. Vestibulum et mauris eget massa molestie auctor. Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.</h5>                    
+                </div> 
+        </div>
+    );
+};
