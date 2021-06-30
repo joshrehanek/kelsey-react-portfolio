bcfc0ed0682084ccff737825673fc0cd9fe59063
Josh Rehanek <joshrehanek@gmail.com>
2021-06-29 22:32:19 -0700

    adds background to hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,33 +1,22 @@
 import React from "react";
 import "../../index.css"
 import "./Hero.css"
+import HeroBackground from "../../assets/images/hero-background.jpg"
 
 export default function Hero() {
     return (
         <div className='hero'>
-            <div className='hero-container'>
-                <section class="banner" id="home">
-                    <div class="text-box">
-                        <h2>Kelsey Camden Smith</h2>
-                        <h3>Behavioral Health Therapist</h3>
-                        <br>
-                        </br>
-                        < img 
-                            src={}
-                        {/* <a
-                            class="link"
-                            href={}
-                            download
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                src="https://img.icons8.com/nolan/64/open-resume.png"
-                                alt="download resume" />
-                        </a> */}
-                    </div>
-                </section>
-            </div>
+            <img
+                src={HeroBackground}
+                alt='green background'
+                />
+            <section class="banner" id="home">
+                <div class="text-box">
+                    <h2>Kelsey Camden Smith</h2>
+                    <h3>Behavioral Health Therapist</h3>
+                </div>
+            </section>
+
         </div>
     );
 }
