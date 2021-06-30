828c4f1bb9362c57cedbbc3de8029deb92f2d267
Josh Rehanek <joshrehanek@gmail.com>
2021-06-29 22:32:00 -0700

    adds Hero section to pages

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import "../../index.css";
-// import Cards from '../Cards/Cards';
-// import Hero from "../Hero/Hero";
-// import Footer from "../Footer/Footer"
+import Hero from "../Hero/Hero";
 // import Bio from "../Bio/Bio"
 
 export default function Home () {
     return (
         <div>
-            stuff and things
-            {/* <Hero />
-            <Bio />
-            <Cards />
-            <Footer /> */}
+            <Hero />
+            {/* <Bio /> */}
+     
         </div>
     )
 }
