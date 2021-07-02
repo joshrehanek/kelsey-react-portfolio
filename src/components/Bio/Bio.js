cf51dc190ff3fa013d6932c0f9ede21fc095ebdd
Josh Rehanek <joshrehanek@gmail.com>
2021-07-01 21:25:18 -0700

    adds education section; consolidates divs

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -3,13 +3,20 @@ import './Bio.css';
 
 export default function Bio() {
     return (
-        <div id="bio" className="bio-container">
-                <div class="heading">
-                    <h2>Bio</h2>
-                </div>
-                <div>
-                    <h5 class="bio-content">Nam quis leo quam. Proin eget tristique purus, eu cursus eros. Etiam ullamcorper eleifend velit id sollicitudin. Curabitur pellentesque neque ligula, et tristique libero fringilla id. Vestibulum et mauris eget massa molestie auctor. Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.</h5>                    
-                </div> 
+        <div>
+            <div class="bio-contaianer">
+                <h2>Bio</h2>
+                <h5 >
+                    Nam quis leo quam. Proin eget tristique purus, eu cursus eros. Etiam ullamcorper eleifend velit id sollicitudin. Curabitur pellentesque neque ligula, et tristique libero fringilla id. Vestibulum et mauris eget massa molestie auctor. Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.
+                </h5>
+            </div>
+
+            <div className='ed-content'>
+                <h2>Education</h2>
+                <h5>
+                    Vestibulum et mauris eget massa molestie auctor. Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.
+                </h5>
+            </div>
         </div>
     );
 };
