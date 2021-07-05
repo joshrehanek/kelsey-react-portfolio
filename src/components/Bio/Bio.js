5a3c8f478f08429eebc94b244acbc195d492d54c
Josh Rehanek <joshrehanek@gmail.com>
2021-07-05 12:35:46 -0700

    adds ids for link connections

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -4,14 +4,14 @@ import './Bio.css';
 export default function Bio() {
     return (
         <div>
-            <div class="bio-contaianer">
+            <div class='bio-contaianer' id='bio'>
                 <h2>Bio</h2>
                 <h5 >
                     Nam quis leo quam. Proin eget tristique purus, eu cursus eros. Etiam ullamcorper eleifend velit id sollicitudin. Curabitur pellentesque neque ligula, et tristique libero fringilla id. Vestibulum et mauris eget massa molestie auctor. Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.
                 </h5>
             </div>
 
-            <div className='ed-content'>
+            <div className='ed-content' id='education'>
                 <h2>Education</h2>
                 <h5>
                     Vestibulum et mauris eget massa molestie auctor. Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.
