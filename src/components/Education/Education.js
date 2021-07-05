3209ea39122e11f7a7d9ac2420a7730a5df6b645
Josh Rehanek <joshrehanek@gmail.com>
2021-07-05 12:43:14 -0700

    adds new education component

---
+++ src/components/Education/Education.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Education.css';
+
+export default function Education() {
+    return (
+        <div>
+            <div className='ed-content' id='education'>
+                <h2>Education</h2>
+                <h5>
+                    Vestibulum et mauris eget massa molestie auctor. Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.
+                </h5>
+            </div>
+        </div>
+    );
+};
