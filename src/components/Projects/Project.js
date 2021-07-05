eef480e8c78a58e9f0bda1092fcf5f7643eeb720
Josh Rehanek <joshrehanek@gmail.com>
2021-07-05 12:43:41 -0700

    adds new projects component

---
+++ src/components/Projects/Project.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Projects.css';
+
+export default function Projects() {
+    return (
+        <div>
+            <div class='projects-contaianer' id='projects'>
+                <h2>Projects</h2>
+                <h5>
+                    Integer odio nisl, consectetur sit amet massa malesuada, rutrum tristique leo. Nulla et metus feugiat, eleifend ex sit amet, gravida tellus. Vivamus pretium neque quis ligula tristique, eu aliquam lorem volutpat. Ut enim metus, egestas nec orci vitae, cursus efficitur risus.
+                </h5>
+            </div>
+        </div>
+    );
+};
