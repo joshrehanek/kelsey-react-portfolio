4be7c98dcfbad3029f0f227456b7520ffd2d011f
Josh Rehanek <joshrehanek@gmail.com>
2021-07-18 23:07:39 -0700

    new test bg & imports profile pic

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "../../index.css"
 import "./Hero.css"
-import HeroBackground from "../../assets/images/hero-background.jpg"
+import HeroBackground from "../../assets/images/blueSky.jpeg"
+import HeroImage from "../../assets/images/kelsSamplePic.jpg"
 
 export default function Hero() {
     return (
         <div className='hero'>
             <img
                 src={HeroBackground}
-                alt='green background'
-                />
-            <section class="banner" id="home">
-                <div class="text-box">
+                alt='sky background'
+            />
+            <section className="banner" id="home">
+                <div className="text-box">
                     <h2>Kelsey Camden Smith</h2>
-                    <h3>Behavioral Health Therapist</h3>
+                    <img
+                        className="samplePic"
+                        src= {HeroImage}
+                        alt= 'sample pic'
+                        />
+                    <h4>Behavioral Health Therapist</h4>
                 </div>
             </section>
 
-        </div>
+        </div >
     );
 }
